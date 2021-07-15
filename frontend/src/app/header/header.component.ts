1c295d5cafd4eb566905cd71a56dfc044484599a
pepe_fabra_valverde <jofaval@outlook.com>
2021-07-15 23:10:46 +0200

    Implement elements array in header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ELEMENTS } from './elements';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  ELEMENTS = ELEMENTS;
 
   constructor() { }
 
