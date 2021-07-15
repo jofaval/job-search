9f4efc6f023fbc1acd6ac1f2e057dacb756b9e49
pepe_fabra_valverde <jofaval@outlook.com>
2021-07-15 23:10:28 +0200

    Create header elements array

---
+++ frontend/src/app/header/elements.ts
@@ -0,0 +1,11 @@
+import { faHome, faSearch, faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+
+/**
+ * Todos los elementos de navegación de la aplicación
+ */
+export const ELEMENTS = [
+    { name: 'Home'   , url: '/', icon: faHome     },
+    { name: 'Buscar' , url: '/', icon: faSearch   },
+    { name: 'Empresa', url: '/', icon: faUserPlus },
+    { name: 'Perfil' , url: '/', icon: faUser     },
+];
