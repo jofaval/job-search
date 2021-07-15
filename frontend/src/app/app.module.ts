78d40d144427116e0d10038a1dbb6d69317a9d35
pepe_fabra_valverde <jofaval@outlook.com>
2021-07-15 19:54:39 +0200

    Create header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
