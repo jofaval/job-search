192919f81d946540a52180796a5275885febd9b4
pepe_fabra_valverde <jofaval@outlook.com>
2021-07-16 10:59:32 +0200

    Create routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BussinessComponent } from './bussiness/bussiness.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home'   , component: HomeComponent      },
+  { path: 'buscar' , component: SearchComponent    },
+  { path: 'empresa', component: BussinessComponent },
+  { path: 'perfil' , component: ProfileComponent   },
+  { path: ''       , component: HomeComponent      },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
